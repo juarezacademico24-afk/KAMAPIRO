2a42bdf1959ae8cf5c3b2eaf04c824d08a8c090c
juarezacademico24-afk <juarezacademico24@gmail.com>
2026-08-22 10:00:19 -0600

    Conectar KAMAPIRO con Firebase

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// KAMAPIRO · Firebase configuration
+window.KAMAPIRO_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBv7BJvJ9Rflar2Bb760vGX65gj-kNy1_xQU",
+  authDomain: "kamapiro.firebaseapp.com",
+  projectId: "kamapiro",
+  storageBucket: "kamapiro.firebasestorage.app",
+  messagingSenderId: "399407290370",
+  appId: "1:399407290370:web:99d9fd3d408a94551af1b"
+};
