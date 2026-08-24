0194f5bf83b7ec1144b1d91029becde0ff3ae62b
juarezacademico24-afk <juarezacademico24@gmail.com>
2026-08-23 23:00:06 -0600

    Restore working Firebase web API key

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,5 @@
 // KAMAPIRO · Firebase configuration
-const KAMAPIRO_API_KEY = "AIzaSyBkvX5d3UKIYMFBzYXE1pakplLghOpdrbw";
+const KAMAPIRO_API_KEY = "AIzaSyBv7BJ9Rf1ar2Bb760vGX65gj-kNy1_xQU";
 window.KAMAPIRO_FIREBASE_CONFIG = {
   apiKey: KAMAPIRO_API_KEY,
   authDomain: "kamapiro.firebaseapp.com",
