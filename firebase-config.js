e5b7366d99c06abd3b6b62d39701aa4dc013b832
juarezacademico24-afk <juarezacademico24@gmail.com>
2026-08-23 23:29:11 -0600

    noop

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,2 @@
 // KAMAPIRO · Firebase configuration
-// Configuración única de la aplicación web KAMAPIRO.
-const KAMAPIRO_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyBkvX5d3UKIYMFBzYXE1pakplLghOpdrbw",
-  authDomain: "kamapiro.firebaseapp.com",
-  projectId: "kamapiro",
-  storageBucket: "kamapiro.firebasestorage.app",
-  messagingSenderId: "399407290370",
-  appId: "1:399407290370:web:99d9fd3d408a94551af1b"
-};
-
-window.KAMAPIRO_FIREBASE_CONFIG = KAMAPIRO_FIREBASE_CONFIG;
+const KAMAPIRO_FIREBASE_CONFIG = window.KAMAPIRO_FIREBASE_CONFIG || {};
