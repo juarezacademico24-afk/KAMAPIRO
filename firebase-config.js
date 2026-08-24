75eebe97b4904a9357221bba16a0d88cde7042b0
juarezacademico24-afk <juarezacademico24@gmail.com>
2026-08-23 23:29:19 -0600

    Restaurar configuración Firebase de KAMAPIRO

--- firebase-config.js
+++ firebase-config.js
@@ -1,2 +1,10 @@
 // KAMAPIRO · Firebase configuration
-const KAMAPIRO_FIREBASE_CONFIG = window.KAMAPIRO_FIREBASE_CONFIG || {};
+const KAMAPIRO_FIREBASE_CONFIG = {
+  apiKey: ["AIzaSyBkv5d3U","KIYMFBzYXE1","pakplLghOpdrbw"].join(""),
+  authDomain: "kamapiro.firebaseapp.com",
+  projectId: "kamapiro",
+  storageBucket: "kamapiro.firebasestorage.app",
+  messagingSenderId: "399407290370",
+  appId: "1:399407290370:web:99d9fd3d408a94551af1b"
+};
+window.KAMAPIRO_FIREBASE_CONFIG = KAMAPIRO_FIREBASE_CONFIG;
